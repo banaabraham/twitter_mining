9c9be85c48421aabffbaf1d781a5a7c1ed5a0505
Bana Abraham <bana.abraham@gmail.com>
2017-09-07 14:38:26 +0700

    Update twitter_mining.py
    added geolocator

--- twitter_mining.py
+++ twitter_mining.py
@@ -1,9 +1,12 @@
-
 import twitter
 import json
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 import numpy as np
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim()
+
 
 CONSUMER_KEY=''
 CONSUMER_SECRET=''
@@ -15,16 +18,10 @@ auth = twitter.oauth.OAuth(OAUTH_TOKEN,OAUTH_TOKEN_SECRET,CONSUMER_KEY,CONSUMER_
 twitter_api = twitter.Twitter(auth=auth)
 
 print (twitter_api)
-WORLD_WOE_ID = 1
-USE_WOE_id = 2
-#world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 
-#print (json.dumps(world_trends, indent=1))
 
-q=['oppo','nokia','xiomi','LG phone','Samsung Phone']
-count = 100
-def twitter_mining(q,count):
-    search_results=twitter_api.search.tweets(q=q,count=count)
+def twitter_mining(q,count,geocode):
+    search_results = twitter_api.search.tweets(q=q,count=count, geocode=geocode)
     statuses = search_results['statuses']
 
     for _ in range(3):
@@ -40,19 +37,34 @@ def twitter_mining(q,count):
     null=0  
     for i in range(len(statuses)):
         blob = TextBlob(str(statuses[i]['text'])).sentiment
+        
         if blob.subjectivity == 0:
             null+=1
         if blob.polarity>0:
             positive+=1           
             sentiment=positive/(len(statuses))
+        if statuses[i]['text']:
+            print(str(statuses[i]['text'])) 
     return sentiment*100 
-s=[]   
-for i in q:
-    s.append(twitter_mining(i,count))
+
+if __name__=='__main__':
+    q=['jokowi']
+    count = 100
+    s=[]
     
-y_pos = np.arange(len(s))
-q=['oppo','nokia','xiomi','LG','Samsung']
-plt.bar(y_pos, s, align='center', alpha=0.5)
-plt.xticks(y_pos, q)
-plt.title('market sentiment (%)')   
+    lokasi = "Jakarta"
+    location = geolocator.geocode(lokasi)
+    radius = 1000
+    geocode = "%f,%f,%dkm" %(location.latitude,location.longitude,radius)    
+        
+    for i in q:
+        s.append(twitter_mining(i,count,geocode))
     
+    y_pos = np.arange(len(s))
+    #q=['jokowi']
+    if len(q)>1:
+        plt.bar(y_pos, s, align='center', alpha=0.5)
+        plt.xticks(y_pos, q)
+        plt.title('public sentiment (%)')
+    elif len(q)==1:
+        print ("%.2f%% " %(s[0]))
